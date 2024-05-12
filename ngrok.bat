6695d529a1c511c1ff48a446a39cde83eefc857d
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2024-05-12 23:46:31 +0800

    Update ngrok.bat

--- ngrok.bat
+++ ngrok.bat
@@ -1,1 +1,1 @@
-docker run --net=host -it -e NGROK_AUTHTOKEN=1nD58LmOYTkT8uZYkxGWtOh3ybl_4xnTEz2CnVwwMgLkvTyR8 ngrok/ngrok:latest http 3838
+docker run --net=host -it -e $NGROK_AUTHTOKEN ngrok/ngrok:latest http 3838
